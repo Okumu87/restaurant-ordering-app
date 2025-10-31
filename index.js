122c76a38baaae29d943f3d0c8100f9a62f9ca5d
okumu87 <okumu793@gmail.com>
2025-10-31 16:23:23 +0100

    mobile ordering app - added border div, script tag, export statement, and fixed jsconfig.json

---
+++ index.js
@@ -0,0 +1,29 @@
+import { menuArray } from './data.js';
+
+const menuContainer = document.getElementById('menu-container');
+
+function renderMenu() {
+    let menuHTML = '';
+    menuArray.forEach((item) => {
+        menuHTML += `
+
+<div class="item">
+       <div class="emoji">${item.emoji}</div>
+        <div class="property">
+            <h3>${item.name}</h3>
+            <p>${item.ingredients.join(', ')}</p>
+            <span>$${item.price}</span>
+        </div>
+          <div class="btn-add__container">
+                <img src="./img/add-btn.png" class="btn-add" alt="add" data-id="${item.id}">
+            </div>
+    </div>
+        `;
+    });
+    if (!menuContainer) return;
+    menuContainer.innerHTML = menuHTML;
+}
+
+renderMenu();                    
+
+
